95cfeb19cf52a5aca53bced903e7e844e06eafdb
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2023-07-22 15:25:25 +0300

    adding USA country list basic

---
+++ BasicCollectionView/BasicCollectionView/BasicCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BasicCollectionViewCell.swift
+//  BasicCollectionView
+//
+//  Created by Adam Khalifa on 22.07.2023.
+//
+
+import UIKit
+
+class BasicCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    
+    
+    
+}
